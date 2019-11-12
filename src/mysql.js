1fe8a6f34807647fc66c26710699f504c256821c
mannix-lei <zx2351634862@outlook.com>
2019-11-12 12:03:55 +0800

    add config

--- src/mysql.js
+++ src/mysql.js
@@ -1,35 +1,35 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'lms123456',
-    database: 'mannix',
-    port: 3306,
-    connectionLimit: 10
-})
+// const pool = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'lms123456',
+//     database: 'mannix',
+//     port: 3306,
+//     connectionLimit: 10
+// })
 
-/**
- * @param {string} sql
- * @param {any} values
- */
-const query = (sql, values) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                return reject(err);
-            } else {
-                connection.query({sql, values, timeout: 60000 }, (err, rows) => {
-                    connection.release();
-                    if (err) {
-                        return reject(err);
-                    } else {
-                        return resolve(rows);
-                    }
-                })
-            }
-        })
-    })
-}
+// /**
+//  * @param {string} sql
+//  * @param {any} values
+//  */
+// const query = (sql, values) => {
+//     return new Promise((resolve, reject) => {
+//         pool.getConnection((err, connection) => {
+//             if (err) {
+//                 return reject(err);
+//             } else {
+//                 connection.query({sql, values, timeout: 60000 }, (err, rows) => {
+//                     connection.release();
+//                     if (err) {
+//                         return reject(err);
+//                     } else {
+//                         return resolve(rows);
+//                     }
+//                 })
+//             }
+//         })
+//     })
+// }
 
-module.exports = query;
+// module.exports = query;
--- src/sql/user.js
+++
@@ -1,8 +0,0 @@
-'use-strict'
-const sqlMap = {
-    findUserById: 'SELECT * FROM user WHERE user_id = ?',
-    findUserByPhone: 'SELECT * FROM user WHERE mobile = ?',
-    findAllUser: 'SELECT * FROM user',
-}
-
-module.exports = sqlMap;
