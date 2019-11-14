ff80fd72396621c1c71c428b60c29f1cadbcf12b
mannix-lei <zx2351634862@outlook.com>
2019-11-14 11:25:12 +0800

    fix port & mysql config

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,7 @@
 const Sequlice = require('sequelize');
 
-const sequelize = new Sequlice('mannix', 'root', 'lms123456', {
-    host: 'localhost',
+const sequelize = new Sequlice('ace', 'root', '123456', {
+    host: '101.200.195.220',
     dialect: 'mysql',
     pool: {
         max: 5,
